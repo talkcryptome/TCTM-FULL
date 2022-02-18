d6132f93dd2dbb29b9061bbeaa526392b5164034
talkcryptome <talkcryptowithme@gmail.com>
2022-02-17 23:29:51 -0700

    Initial Commit

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
 import { ModalProvider } from '@pancakeswap-libs/uikit'
 // import bsc, { UseWalletProvider } from '@binance-chain/bsc-use-wallet'
 import * as bsc from '@binance-chain/bsc-use-wallet'
@@ -8,14 +9,21 @@ import { LanguageContextProvider } from 'contexts/Localisation/languageContext'
 import { ThemeContextProvider } from 'contexts/ThemeContext'
 import { BlockContextProvider } from 'contexts/BlockContext'
 import { RefreshContextProvider } from 'contexts/RefreshContext'
-import store from 'state'
+import { NetworkContextName } from './constants'
+import getLibrary from './utils/getLibrary'
+import store from './state'
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
 const Providers: React.FC = ({ children }) => {
   const rpcUrl = getRpcUrl()
   const chainId = parseInt(process.env.REACT_APP_CHAIN_ID)
   return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+    <Web3ProviderNetwork getLibrary={getLibrary}>
     <Provider store={store}>
       <ThemeContextProvider>
+      <ModalProvider>{children}</ModalProvider>
         <LanguageContextProvider>
           <bsc.UseWalletProvider
             chainId={chainId}
@@ -33,6 +41,8 @@ const Providers: React.FC = ({ children }) => {
         </LanguageContextProvider>
       </ThemeContextProvider>
     </Provider>
+    </Web3ProviderNetwork>
+    </Web3ReactProvider>
   )
 }
 
--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -8,9 +8,25 @@ import { poolsConfig } from 'config/constants'
 import erc20 from 'config/abi/erc20.json'
 import masterChef from 'config/abi/masterchef.json'
 import sousChef from 'config/abi/sousChef.json'
+// From Swap
+import { Contract } from '@ethersproject/contracts'
+import { ChainId, WETH } from '@pancakeswap-libs/sdk'
+import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
+import { useMemo } from 'react'
+import ENS_ABI from '../constants/abis/ens-registrar.json'
+import ENS_PUBLIC_RESOLVER_ABI from '../constants/abis/ens-public-resolver.json'
+import { ERC20_BYTES32_ABI } from '../constants/abis/erc20'
+import ERC20_ABI from '../constants/abis/erc20.json'
+import { MIGRATOR_ABI, MIGRATOR_ADDRESS } from '../constants/abis/migrator'
+import UNISOCKS_ABI from '../constants/abis/unisocks.json'
+import WETH_ABI from '../constants/abis/weth.json'
+import { MULTICALL_ABI, MULTICALL_NETWORKS } from '../constants/multicall'
+import { V1_EXCHANGE_ABI, V1_FACTORY_ABI, V1_FACTORY_ADDRESSES } from '../constants/v1'
+import { getContract } from '../utils'
+import { useActiveWeb3React } from './index'
 
 
-const useContract = (abi: AbiItem, address: string, contractOptions?: ContractOptions) => {
+const useContract2 = (abi: AbiItem, address: string, contractOptions?: ContractOptions) => {
   const web3 = useWeb3()
   const [contract, setContract] = useState(new web3.eth.Contract(abi, address, contractOptions))
 
@@ -27,7 +43,7 @@ const useContract = (abi: AbiItem, address: string, contractOptions?: ContractOp
 
 export const useERC20 = (address: string) => {
   const erc20Abi = (erc20 as unknown) as AbiItem
-  return useContract(erc20Abi, address)
+  return useContract2(erc20Abi, address)
 }
 
 export const useCake = () => {
@@ -36,14 +52,91 @@ export const useCake = () => {
 
 export const useMasterchef = () => {
   const abi = (masterChef as unknown) as AbiItem
-  return useContract(abi, getMasterChefAddress())
+  return useContract2(abi, getMasterChefAddress())
 }
 
 export const useSousChef = (id) => {
   const config = poolsConfig.find((pool) => pool.sousId === id)
   const rawAbi = sousChef
   const abi = (rawAbi as unknown) as AbiItem
-  return useContract(abi, config.contractAddress[process.env.REACT_APP_CHAIN_ID])
+  return useContract2(abi, config.contractAddress[process.env.REACT_APP_CHAIN_ID])
 }
 
-export default useContract
+export default useContract2
+
+// From Swap
+
+// returns null on errors
+function useContract(address: string | undefined, ABI: any, withSignerIfPossible = true): Contract | null {
+  const { library, account } = useActiveWeb3React()
+
+  return useMemo(() => {
+    if (!address || !ABI || !library) return null
+    try {
+      return getContract(address, ABI, library, withSignerIfPossible && account ? account : undefined)
+    } catch (error) {
+      console.error('Failed to get contract', error)
+      return null
+    }
+  }, [address, ABI, library, withSignerIfPossible, account])
+}
+
+export function useV1FactoryContract(): Contract | null {
+  const { chainId } = useActiveWeb3React()
+  return useContract(chainId && V1_FACTORY_ADDRESSES[chainId], V1_FACTORY_ABI, false)
+}
+
+export function useV2MigratorContract(): Contract | null {
+  return useContract(MIGRATOR_ADDRESS, MIGRATOR_ABI, true)
+}
+
+export function useV1ExchangeContract(address?: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract(address, V1_EXCHANGE_ABI, withSignerIfPossible)
+}
+
+export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract(tokenAddress, ERC20_ABI, withSignerIfPossible)
+}
+
+export function useWETHContract(withSignerIfPossible?: boolean): Contract | null {
+  const { chainId } = useActiveWeb3React()
+  return useContract(chainId ? WETH[chainId].address : undefined, WETH_ABI, withSignerIfPossible)
+}
+
+export function useENSRegistrarContract(withSignerIfPossible?: boolean): Contract | null {
+  const { chainId } = useActiveWeb3React()
+  let address: string | undefined
+  if (chainId) {
+    switch (chainId) {
+      case ChainId.MAINNET:
+      case ChainId.BSCTESTNET:
+    }
+  }
+  return useContract(address, ENS_ABI, withSignerIfPossible)
+}
+
+export function useENSResolverContract(address: string | undefined, withSignerIfPossible?: boolean): Contract | null {
+  return useContract(address, ENS_PUBLIC_RESOLVER_ABI, withSignerIfPossible)
+}
+
+export function useBytes32TokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract(tokenAddress, ERC20_BYTES32_ABI, withSignerIfPossible)
+}
+
+export function usePairContract(pairAddress?: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract(pairAddress, IUniswapV2PairABI, withSignerIfPossible)
+}
+
+export function useMulticallContract(): Contract | null {
+  const { chainId } = useActiveWeb3React()
+  return useContract(chainId && MULTICALL_NETWORKS[chainId], MULTICALL_ABI, false)
+}
+
+export function useSocksController(): Contract | null {
+  const { chainId } = useActiveWeb3React()
+  return useContract(
+    chainId === ChainId.MAINNET ? '0x65770b5283117639760beA3F867b69b3697a91dd' : undefined,
+    UNISOCKS_ABI,
+    false
+  )
+}
